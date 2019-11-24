7e80c08a581ba86141f3aabb90c05538062955b6
Brian Bird <birdb@lanecc.edu>
2019-11-24 14:36:53 -0800

    Completed the fake repository--AddReview passes

--- GoodBookNook/Repositories/FakeBookRepository.cs
+++ GoodBookNook/Repositories/FakeBookRepository.cs
@@ -9,10 +9,6 @@ namespace GoodBookNook.Repositories
         private  List<Book> books = new List<Book>();
         public  List<Book> Books { get { return books; } }
 
-         public FakeBookRepository()
-        {
-            AddTestData();
-        }
 
         public  void AddBook(Book book)
         {
@@ -21,53 +17,14 @@ namespace GoodBookNook.Repositories
 
         public void AddReview(Book book, Review review)
         {
-            //TODO finish this method
+            book.Reviews.Add(review);
         }
+
         public  Book GetBookByTitle(string title)
         {
             Book book = books.Find(b => b.Title == title);
             return book;
         }
 
-        void AddTestData()
-        {
-            Book book = new Book()
-            {
-                Title = "The Fellowship of the Ring",
-                PubDate = new DateTime(1937, 1, 1)
-            };
-            book.Authors.Add(new Author
-            {
-                Name = "J.R.R. Tolkein"
-            }
-            );
-            books.Add(book);
-
-            book = new Book()
-            {
-                Title = "Book2",
-                PubDate = new DateTime(1970, 1, 1)
-            };
-            book.Authors.Add(new Author
-            {
-                Name = "Author 2"
-            }
-            );
-            Review review = new Review() { ReviewText = "Awesome book!" };
-            book.Reviews.Add(review);
-            books.Add(book);
-
-            book = new Book()
-            {
-                Title = "Another Book",
-                PubDate = new DateTime(2000, 1, 1)
-            };
-            book.Authors.Add(new Author
-            {
-                Name = "Unknown"
-            }
-            );
-            books.Add(book);
-        }
     }
 }
