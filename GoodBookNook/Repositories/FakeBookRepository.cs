ad63019d8e56cb2c00d0242fa5211fd95c082a70
Brian Bird <birdb@lanecc.edu>
2019-11-23 17:58:32 -0800

    Added another unit test
    And refactored the fake repository so that the test book objects are created in the test.

--- GoodBookNook/Repositories/FakeBookRepository.cs
+++ GoodBookNook/Repositories/FakeBookRepository.cs
@@ -11,11 +11,6 @@ namespace GoodBookNook.Repositories
         private  List<Book> books = new List<Book>();
         public  List<Book> Books { get { return books; } }
 
-         public FakeBookRepository()
-        {
-            AddTestData();
-        }
-
         public  void AddBook(Book book)
         {
             books.Add(book);
@@ -27,45 +22,5 @@ namespace GoodBookNook.Repositories
             return book;
         }
 
-        void AddTestData()
-        {
-            Book book = new Book()
-            {
-                Title = "The Fellowship of the Ring",
-                PubDate = new DateTime(1937, 1, 1)
-            };
-            book.Authors.Add(new Author
-            {
-                Name = "J.R.R. Tolkein"
-            }
-            );
-            books.Add(book);
-
-            book = new Book()
-            {
-                Title = "Book2",
-                PubDate = new DateTime(1970, 1, 1)
-            };
-            book.Authors.Add(new Author
-            {
-                Name = "Author 2"
-            }
-            );
-            Review review = new Review() { ReviewText = "Awesome book!" };
-            book.Reviews.Add(review);
-            books.Add(book);
-
-            book = new Book()
-            {
-                Title = "Another Book",
-                PubDate = new DateTime(2000, 1, 1)
-            };
-            book.Authors.Add(new Author
-            {
-                Name = "Unknown"
-            }
-            );
-            books.Add(book);
-        }
     }
 }
