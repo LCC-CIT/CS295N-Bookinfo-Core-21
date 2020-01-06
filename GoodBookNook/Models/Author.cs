ac3922b288a019463eff3cefb7b417c3c9a9d357
Brian Bird <birdb@lanecc.edu>
2020-01-06 11:12:03 -0800

    Added more validation

--- GoodBookNook/Models/Author.cs
+++ GoodBookNook/Models/Author.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GoodBookNook.Models
 {
     public class Author
     {
         public int AuthorID { get; set; }
+        [Required]
         public string Name { get; set; }
         public DateTime Birthday { get; set; }
     }
